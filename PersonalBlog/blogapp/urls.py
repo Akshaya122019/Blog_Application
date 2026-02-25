64c36ef0d13d913e5fb4d53dd5073ffa1bcb72e5
Akshaya122019 <geoakshay2002@gmail.com>
2026-02-25 21:25:22 +0530

    authentication

--- PersonalBlog/blogapp/urls.py
+++ PersonalBlog/blogapp/urls.py
@@ -3,6 +3,13 @@ from . import views
 
 urlpatterns = [
     path('',views.Dashboard,name='dashboard'),
-    path('login/',views.LoginView,name='login')
+    
+    path('login/',views.LoginView,name='login'),
+    path('logout',views.LogoutUser, name='logout'),
+
+    path('Add_User/', views.add_user, name='add_user'),
+    path('update/<int:user_id>/', views.update_user, name='update_user'),
+    path('users/delete/<int:user_id>/', views.delete_user, name='delete_user'),
+    path('User/', views.show_user, name='user'),
    
 ]
--- PersonalBlog/blogapp/views.py
+++ PersonalBlog/blogapp/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.http import HttpResponseForbidden
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def Dashboard(request):
     return render(request,"dashboard.html")
+
 
 def LoginView(request):
     if request.method == 'POST':
         username = request.POST.get('username')
-        password = request.POST.get('passoword')
+        password = request.POST.get('password')
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request,user)
@@ -21,7 +25,84 @@ def LoginView(request):
             return redirect('login')
     return render(request,"login.html")
 
+@login_required
 def add_user(request):
     if not request.user.groups.filter(name='Admin').exists():
         return HttpResponseForbidden("You are not authorized to access this page")
+    if request.method == 'POST':
+        username = request.POST['username'].strip()
+        email = request.POST['email'].strip()
+        password = request.POST['password']
+        confirm = request.POST['confirm']
+        role = request.POST['role']
 
+        if password != confirm:
+            messages.error(request,"Password do not match")
+            return redirect('add_user')
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already exists")
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+
+        if role == 'admin':
+            group, _ = Group.objects.get_or_create(name='Admin')
+        elif role == 'user':
+            group, _ = Group.objects.get_or_create(name='User')
+        else:
+            messages.warning(request, "Invalid role selected")
+            return redirect('add_user')
+        
+        user.groups.add(group)
+
+        messages.success(request, f"Account created successfully as {role.capitalize()}")
+        return redirect('user')
+    
+    return render(request, 'add_user.html')
+
+def show_user(request):
+    if not request.user.groups.filter(name='Admin').exists():
+        return HttpResponseForbidden("You are not authorized to access this page")
+    
+    users = User.objects.all()
+    is_admin = request.user.groups.filter(name='Admin').exists()
+
+    return render(request, 'view_user.html', {"users":users, "is_admin":is_admin})
+
+@login_required
+def LogoutUser(request):
+    logout(request)
+    return redirect("login")
+
+@login_required
+def update_user(request, user_id):
+    if not request.user.groups.filter(name='Admin').exists():
+        return HttpResponseForbidden("You are not authorized to access this page")
+    
+    user = get_object_or_404(User,id=user_id)
+    current_role = user.groups.first().name if user.groups.exists() else "User"
+
+    if request.method == 'POST':
+        user.username = request.POST['username']
+        user.email = request.POST['email']
+        user.save()
+
+        new_role = request.POST['role']
+        user.groups.clear()
+        group, _ = Group.objects.get_or_create(name=new_role)
+        user.groups.add(group)
+
+        messages.success(request, "User Updated Successfully")
+        return redirect('user')
+    return render(request, 'update_user.html', {'user': user, 'user_role': current_role})
+
+
+def delete_user(request, user_id):
+    if not request.user.groups.filter(name='Admin').exists():
+        return HttpResponseForbidden("You are not authorized to access this page.")
+    
+    user = get_object_or_404(User, id=user_id)
+    user.delete()
+    messages.success(request, 'User deleted successfully')
+    return redirect('user')
